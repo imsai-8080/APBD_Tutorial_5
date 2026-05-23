6c2a6cde8e91ffb3e2909595da248aef8ff83d18
imsai-8080 <s32304@pjwstk.edu.pl>
2026-05-23 19:04:07 +0200

    uzupełnienie zawartości klas PCResponseDto i PCComponentResponseDto

--- APBD_Tutorial_5/DTOs/PCComponentResponseDto.cs
+++ APBD_Tutorial_5/DTOs/PCComponentResponseDto.cs
@@ -2,5 +2,7 @@
 
 public class PCComponentResponseDto
 {
-    
+    public string ComponentCode { get; set; }
+    public string ComponentName { get; set; }
+    public int Amount { get; set; }
 }
--- APBD_Tutorial_5/DTOs/PCResponseDto.cs
+++ APBD_Tutorial_5/DTOs/PCResponseDto.cs
@@ -2,5 +2,10 @@
 
 public class PCResponseDto
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
 }
