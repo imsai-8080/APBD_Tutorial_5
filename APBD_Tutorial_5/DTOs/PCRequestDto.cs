84a813efb71d2f38eed1c84f72f70c69dc3d934c
imsai-8080 <s32304@pjwstk.edu.pl>
2026-05-23 18:52:31 +0200

    uzupełnienie klasy PCRequestDto

--- APBD_Tutorial_5/DTOs/PCRequestDto.cs
+++ APBD_Tutorial_5/DTOs/PCRequestDto.cs
@@ -1,6 +1,24 @@
-﻿namespace APBD_Tutorial_5.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD_Tutorial_5.DTOs;
 
 public class PCRequestDto
 {
-    
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [Range(0.1, 1000)]
+    public double Weight { get; set; }
+
+    [Required]
+    public int Warranty { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
 }
